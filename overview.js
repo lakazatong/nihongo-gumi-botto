181df60e15cc78822bf054e7e38d4f9d0ea248ba
lakazatong <Bo_wood@outlook.fr>
2025-05-05 23:40:40 +0200

    Update overview.js

--- overview.js
+++ overview.js
@@ -1,37 +1,35 @@
 const fs = require("fs");
 const path = require("path");
-const { SlashCommandBuilder } = require("discord.js");
-
-const commandsDir = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsDir).filter((file) => file.endsWith(".js"));
 
 const overview = [];
 
-commandFiles.forEach((file) => {
-	const command = require(path.join(commandsDir, file));
-	const commandName = command.data.name;
-	const commandDescription = command.data.description;
-	let options = "";
+fs.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"))
+	.forEach((file) => {
+		const command = require(`./commands/${file}`);
+		const commandName = command.data.name;
+		const commandDescription = command.data.description;
+		let options = "";
 
-	if (command.data.options?.length > 0) {
-		options = command.data.options
-			.map((option) => {
-				const optionName = option.name;
-				const optionDescription = option.description;
-				const optionRequired = option.required ? "Required" : "Optional";
-				return `- ${optionName}: ${optionDescription} (${optionRequired})`;
-			})
-			.join("\n");
+		if (command.data.options?.length > 0) {
+			options = command.data.options
+				.map((option) => {
+					const optionName = option.name;
+					const optionDescription = option.description;
+					const optionRequired = option.required ? "Required" : "Optional";
+					return `- ${optionName}: ${optionDescription} (${optionRequired})`;
+				})
+				.join("\n");
 
-		options = `Options:\n${options}`;
-	}
+			options = `Options:\n${options}`;
+		}
 
-	overview.push({
-		name: commandName,
-		description: commandDescription,
-		options: options,
+		overview.push({
+			name: commandName,
+			description: commandDescription,
+			options: options,
+		});
 	});
-});
 
 const overviewText = overview
 	.map((cmd) => {
